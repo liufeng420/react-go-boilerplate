b20e05dfa1455764a76387dec86ac1b747af2350
MrHuxu <hxtheone@gmail.com>
2017-09-30 14:26:20 +0800

    update rollup configuration to make it work

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,13 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import css from 'rollup-plugin-css-only';
+import json from 'rollup-plugin-json';
+import commonjs from 'rollup-plugin-commonjs';
+import uglify from 'rollup-plugin-uglify';
+import { minify } from 'uglify-es';
 
 export default {
-  entry   : 'client/index.js',
+  input   : 'client/index.js',
   format  : 'cjs',
   plugins : [
     resolve({
@@ -12,7 +16,18 @@ export default {
     babel({
       exclude : 'node_modules/**'
     }),
-    css({ output: 'client/bundle.css' })
+    css({ output: 'client/bundle.css' }),
+    json(),
+    commonjs({
+      namedExports : {
+        'node_modules/react-dom/index.js'             : [ 'render', 'findDOMNode' ],
+        'node_modules/react/index.js'                 : [ 'Component', 'Children', 'createElement', 'cloneElement' ],
+        'node_modules/radium/lib/index.js'            : [ 'Style' ],
+        'node_modules/prop-types/index.js'            : [ 'shape', 'bool', 'object', 'string', 'func' ],
+        'node_modules/rc-editor-mention/lib/index.js' : [ 'toString' ]
+      }
+    }),
+    uglify({}, minify)
   ],
   dest : 'client/bundle.js'
 };
